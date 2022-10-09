5f86bc597e491dcf8a6a4c2a7ce97a068e885213
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-09 17:24:08 +0530

    Time: 10 ms (21.41%), Space: 51.2 MB (61.24%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -13,67 +13,23 @@
  *     }
  * }
  */
-
-class BSTInorder {
-    private Stack<TreeNode> left, right;
-    
-    BSTInorder(TreeNode root) {
-        left = new Stack();
-        right = new Stack();
-        pushAllLeft(root);
-        pushAllRight(root);
-    }
-    
-    private void pushAllLeft(TreeNode root) {
-        TreeNode temp = root;
-        while(temp != null) {
-            left.push(temp);
-            temp = temp.left;
-        }
-    }
-    
-    private void pushAllRight(TreeNode root) {
-        TreeNode temp = root;
-        while(temp != null) {
-            right.push(temp);
-            temp = temp.right;
-        }
-    }
-    
-    private int next() {
-        TreeNode top = left.pop();
-        pushAllLeft(top.right);
-        return top.val;
-    }
-    
-    private int before() {
-        TreeNode top = right.pop();
-        pushAllRight(top.left);
-        return top.val;
-    }
-    
-    public boolean findTarget(int k) {
-        int l = next();
-        int r = before();
-        while(l < r) {
-            int sum = l+r;
-            if(sum == k) {
-                return true;
-            }
-            if(sum > k) {
-                r = before();
-            }
-            else {
-                l = next();
-            }
-        }
-        return false;
-    }
-}
-
 class Solution {
+    HashSet<Integer> seen;
     public boolean findTarget(TreeNode root, int k) {
-        BSTInorder bstInorder = new BSTInorder(root);
-        return bstInorder.findTarget(k);
+        seen = new HashSet();
+        return dfs(root, k);
     }
+    
+    private boolean dfs(TreeNode root, int target) {
+        if(root == null) {
+            return false;
+        }
+        
+        if(seen.contains(target - root.val)) {
+            return true;
+        }
+        seen.add(root.val);
+        return dfs(root.left, target) || dfs(root.right, target);
+    } 
+    
 }
