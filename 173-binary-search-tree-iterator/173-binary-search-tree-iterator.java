ef4007452fa0f8261e2a5431fb84e7e0a2bf8263
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-09 12:04:50 +0530

    Time: 39 ms (28.41%), Space: 51.7 MB (63.45%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
@@ -15,7 +15,7 @@
  */
 class BSTIterator {
     private Stack<TreeNode> inorder;
-    
+    private List<Integer> inorderTraversal;
     private void insertAllLeft(TreeNode node) {
         while(node != null) {
             inorder.push(node);
@@ -25,21 +25,36 @@ class BSTIterator {
     
     public BSTIterator(TreeNode root) {
         inorder = new Stack();
+        inorderTraversal = new ArrayList();
+        insertAll(root);
         insertAllLeft(root);
     }
     
     public int next() {
-        TreeNode top = inorder.pop();
-        if(top.right != null) {
-            TreeNode node = top.right;
-            insertAllLeft(node);
-        }
+//         TreeNode top = inorder.pop();
+//         if(top.right != null) {
+//             TreeNode node = top.right;
+//             insertAllLeft(node);
+//         }
         
-        return top.val;
+//         return top.val;
+        int nextVal = inorderTraversal.get(0);
+        inorderTraversal.remove(0);
+        return nextVal;
     }
     
     public boolean hasNext() {
-        return !inorder.isEmpty();
+        // return !inorder.isEmpty();
+        return !inorderTraversal.isEmpty();
+    }
+    
+    private void insertAll(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        insertAll(root.left);
+        inorderTraversal.add(root.val);
+        insertAll(root.right);
     }
 }
 
