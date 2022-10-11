4ecd587ee6bb68538ec380b7014889779339834c
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-11 10:18:23 +0530

    Time: 1 ms (82.47%), Space: 43.2 MB (40.17%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -21,17 +21,15 @@ class Solution {
             return list1;
         }
         
-        ListNode dummy = new ListNode(-1);
         if(list1.val < list2.val) {
-            dummy.next = list1;
-            list1 = list1.next;
+            list1.next = mergeTwoLists(list1.next, list2);
         }
         else {
-            dummy.next = list2;
-            list2 = list2.next;
+            ListNode nextNode = list1;
+            list1 = list2;
+            list1.next = mergeTwoLists(nextNode, list2.next);
         }
-        dummy = dummy.next;
-        dummy.next = mergeTwoLists(list1, list2);
-        return dummy;
+        
+        return list1;
     }
 }
