929f0a3c32ddf9b061aae567852bc0042c06c10a
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-11 12:25:46 +0530

    Time: 21 ms (40.59%), Space: 54.3 MB (34.82%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition-sort.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int n = nums.length;
+        Arrays.sort(nums);
+        int pairSum = 0;
+        for(int i = 0; i<n-1; i+=2) {
+            int curr = nums[i];
+            int next = nums[i+1];
+            int min = Math.min(curr, next);
+            pairSum += min;
+        }
+        
+        return pairSum;
+    }
+}
+
+/* 
+- TC: O(n logn)
+- SC: O(1)
+*/
+
+/*
+[(1,3), (2,4)] = 3
+[(1,2), (3,4)] = 4
+[(1,4), (2,3)] = 2
+[1,2,3,4]
+
+[6,2,6,5,1,2]
+[1,2,2,5,6,6]
+[(1,2),(2,5),(6,6)] = 1+2+5 = 
+*/
