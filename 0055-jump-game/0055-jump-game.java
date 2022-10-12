80031a0b1eafef428f8976a33f1b6ecd7a2ae189
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-12 12:52:38 +0530

    Time: 93 ms (26.22%), Space: 42.4 MB (99.66%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return false;
+        }
+        
+        int n = nums.length;
+       
+        int[] ways = new int[n];
+        if(n == 1) {
+            return true;
+        }
+        if(nums[0] == 0) {
+            return false;
+        }
+        ways[0] = 1;
+        for(int i = 0; i<n; i++) {
+            int maxJump = nums[i];
+            if(ways[i] == 0) {
+                continue;
+            }
+            for(int jump = 1; jump<=maxJump; jump++) {
+                int nextJump = jump+i;
+                if(nextJump >= n) {
+                    break;
+                }
+                if(nextJump == n-1) {
+                    return true;
+                }
+                ways[nextJump]++;
+            }
+        }
+        return ways[n-1] != 0;
+    }
+    
+    private boolean canJumpHelper(int index, int n, int[] nums) {
+        if(index >= n-1) {
+            return true;
+        }
+        
+        int jump = 1;
+        int maxJump = nums[index];
+        if(maxJump == 0) {
+            return false;
+        }
+        while(jump <= maxJump && jump < n) {
+            int nextJump = index + jump;
+            if(canJumpHelper(nextJump, n, nums)) {
+                return true;
+            }
+            jump++;
+        }
+        
+        return false;
+    }
+}
