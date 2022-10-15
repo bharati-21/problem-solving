b5418cef644f4622d7056e6ef8a727c223373859
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-15 13:35:39 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (8.99%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 0) {
+            return 0;
+        }
+        
+        int[] tabulation = new int[n+1];
+        if(n <= 2) {
+            return n;
+        }
+        
+        tabulation[1] = 1;
+        tabulation[2] = 2;
+
+        for(int i = 3; i<=n; i++) {
+            int first = tabulation[i-1];
+            int second = tabulation[i-2];
+            
+            tabulation[i] = first + second;
+        }
+        
+        return tabulation[n];
+    }
+}
