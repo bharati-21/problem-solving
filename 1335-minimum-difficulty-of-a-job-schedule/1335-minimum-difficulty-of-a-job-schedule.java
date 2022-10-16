5be9342e1dd5b911bee2ed1ef4046fa234eff322
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-16 10:32:42 +0530

    Time: 162 ms (7.45%), Space: 42.4 MB (30.14%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -5,37 +5,35 @@ class Solution {
             return -1;
         }
         
-        int[][][] memo = new int[n+1][n+1][d+1];
-        for(int[][] row1: memo) {
-            for(int[] row: row1) {
-                Arrays.fill(row, Integer.MAX_VALUE);
-            }
+        int[][] memo = new int[n+1][d+1];
+        for(int[] row: memo) {
+            Arrays.fill(row, Integer.MAX_VALUE);
         }
         return minDifficultyHelper(jobDifficulty, d, 0, n, memo);
     }
         
     // 
-    private int minDifficultyHelper(int[] jobDifficulty, int d, int start, int end, int[][][] memo) {
+    private int minDifficultyHelper(int[] jobDifficulty, int d, int start, int end, int[][] memo) {
         if(d == 1) {
             // return the max at this value from start to end
             return getMax(jobDifficulty, start, end-1);
         }
         
-        if(memo[start][end][d] != Integer.MAX_VALUE) {
-            return memo[start][end][d];
+        if(memo[start][d] != Integer.MAX_VALUE) {
+            return memo[start][d];
         }
         
-        int minValue = memo[start][end][d];
+        int minValue = memo[start][d];
         for(int i = start; i<=end-d; i++) {
             // This gets the values at the cut [i+1, end)
             int nextCut = minDifficultyHelper(jobDifficulty, d-1, i+1, end, memo);
             int currentCutMax = getMax(jobDifficulty, start, i);
                 
             minValue = Math.min(minValue, (nextCut + currentCutMax));
-            memo[start][i][d] = minValue;
+            memo[start][d] = minValue;
         }
         
-        return memo[start][end][d] = minValue;
+        return memo[start][d] = minValue;
     }
     
     private int getMax(int[] jobDifficulty, int i, int j) {
