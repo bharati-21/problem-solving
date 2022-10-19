13eda5231060c01dd710c75a6f1ff7ca97705edb
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-19 09:13:25 +0530

    Time: 16 ms (17.88%), Space: 45.3 MB (42.39%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,37 +1,60 @@
+class Pair {
+    String s;
+    int f;
+    Pair(String s, int f) {
+        this.s = s;
+        this.f = f;
+    }
+}
+
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         List<String> frequentWords = new ArrayList();
         
+        HashMap<String, Integer> count = new HashMap();
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new CustomComparator());
         
-        HashMap<String, Integer> map = new HashMap();
-        if(words == null || words.length == 0) {
-            return frequentWords;
-        }
-        
-        int mostFreq = 0;
         for(String word: words) {
-            if(!map.containsKey(word)) {
-                map.put(word, 0);
-            }
-            map.put(word, map.get(word)+1);
-            mostFreq = Math.max(mostFreq, map.get(word));
+            if(!count.containsKey(word)) {
+                count.put(word, 0);
+            }    
+            count.put(word, count.get(word)+1);
         }
         
-        Arrays.sort(words);
-        String prevWord = "";
-        while(mostFreq > 0 && frequentWords.size() < k) {
-            for(String word: words) {
-                if(map.get(word) == mostFreq && !prevWord.equals(word)) {
-                    frequentWords.add(word);
-                    prevWord = word;
-                    if(frequentWords.size() == k) {
-                        break;
-                    }
+        for(String s: count.keySet()) {
+            int f = count.get(s);
+            Pair p = new Pair(s,f);
+            if(pq.size() < k) {
+                pq.offer(p);
+            }
+            else {
+                Pair peek = pq.peek();
+                if(peek.f != f && f > peek.f) {
+                    pq.poll();
+                    pq.offer(p);
+                }
+                else if(peek.f == f && peek.s.compareTo(s) > 0) {
+                    pq.poll();
+                    pq.offer(p);
                 }
             }
-            mostFreq--;
+        }
+        
+        int i = 0;
+        while(i < k) {
+            frequentWords.add(0,pq.poll().s);
+            i++;
         }
         
         return frequentWords;
+    }
+}
+
+class CustomComparator implements Comparator<Pair> {
+    public int compare(Pair a, Pair b) {
+        if(a.f != b.f) {
+            return a.f - b.f;
+        }
+        return b.s.compareTo(a.s);
     }
 }
