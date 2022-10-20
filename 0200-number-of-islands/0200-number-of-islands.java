6a652b9c93a822822c258c75043455fe2f20bb58
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-20 12:02:02 +0530

    Time: 9 ms (35.10%), Space: 56.1 MB (78.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -14,13 +14,45 @@ class Solution {
                 int cell = grid[r][c];
                 boolean seen = visited[r][c];
                 if(cell == '1' && !seen) {
-                    dfs(r, c, m, n, grid, visited);
+                    bfs(r, c, m, n, grid, visited);
                     islands++;
                 }
             }
         }
         
         return islands;
+    }
+    
+    private void bfs(int r, int c, int m, int n, char[][] grid, boolean[][] visited) {
+        Queue<int []> queue = new LinkedList();
+        
+        visited[r][c] = true;
+        queue.add(new int[] {r,c});
+        
+        int[][] directions = new int[][] {
+            {-1,0}, {1,0}, {0,-1}, {0,1}
+        };
+        
+        while(!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            
+            for(int[] dir: directions) {
+                int nextRow = dir[0] + cell[0];
+                int nextCol = dir[1] + cell[1];
+                if(isValid(nextRow, nextCol, m, n, grid, visited)) {
+                    visited[nextRow][nextCol] = true;
+                    queue.add(new int[] { nextRow, nextCol });
+                }
+            }
+        }
+    }
+    
+    private boolean isValid(int r, int c, int m, int n, char[][] grid, boolean[][] visited) {
+        if(r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == '0' || visited[r][c]) {
+            return false;
+        }
+        
+        return true;
     }
     
     private void dfs(int r, int c, int m, int n, char[][] grid, boolean[][] visited) {
