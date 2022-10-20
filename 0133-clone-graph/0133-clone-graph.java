2c7c98fc465ec8412919f4705cfec8bcfa875a11
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-20 18:52:40 +0530

    Time: 26 ms (91.64%), Space: 42.2 MB (96.01%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -27,49 +27,92 @@ class Solution {
         Node head = new Node(node.val, new ArrayList());
         
         // This maps created nodes
-        Map<Integer, Node> map = new HashMap();
-        map.put(node.val, head);
+        Map<Node, Node> map = new HashMap();
+        map.put(node, head);
         
         Set<Node> seen = new HashSet();
-        Queue<Node> queue = new LinkedList();
+//         Queue<Node> queue = new LinkedList();
         
-        queue.add(node);
-        queue.add(head);
-        seen.add(node);
+//         queue.add(node);
+//         queue.add(head);
+//         seen.add(node);
         
-        while(!queue.isEmpty()) {
-            // This is the node for old graph
-            Node prev = queue.poll();
-            // this represents the old graph node
-            Node curr = queue.poll();
+//         while(!queue.isEmpty()) {
+//             // This is the node for old graph
+//             Node prev = queue.poll();
+//             // this represents the old graph node
+//             Node curr = queue.poll();
             
-            if(prev == null) {
-                continue;
+//             if(prev == null) {
+//                 continue;
+//             }
+            
+//             for(Node n: prev.neighbors) {
+//                 int val = n.val;
+//                 if(!seen.contains(n)) {
+//                     Node newNode;
+//                     if(map.containsKey(val)) {
+//                         newNode = map.get(val);
+//                     }
+//                     else {
+//                         newNode = new Node(val, new ArrayList());
+//                         map.put(val, newNode);
+//                     }
+                    
+//                     seen.add(n);
+//                     queue.add(n);
+//                     queue.add(newNode);
+//                 }
+                
+//                 if(!curr.neighbors.contains(map.get(val))) {
+//                     curr.neighbors.add(map.get(val));
+//                 }
+//             }
+//         }
+        
+        // return head;
+        
+        return dfs(node, map, seen);
+    }
+    
+    
+    private Node dfs(Node node, Map<Node, Node> map, Set<Node> visited) {
+        if(node == null) {
+            return null;
+        }
+        
+        
+        visited.add(node);
+         
+        Node curr;
+        if(!map.containsKey(node)) {
+            curr = new Node(node.val, new ArrayList());
+            map.put(node, curr);
+        }
+        else {
+            curr = map.get(node);
+        }
+        
+        // iterate over neighbors
+        for(Node neighbor: node.neighbors) {
+            Node newNeighbor;
+            if(!map.containsKey(neighbor)) {
+                newNeighbor = new Node(neighbor.val, new ArrayList());
+                map.put(neighbor, newNeighbor);
+            }
+            else {
+                newNeighbor = map.get(neighbor);
             }
             
-            for(Node n: prev.neighbors) {
-                int val = n.val;
-                if(!seen.contains(n)) {
-                    Node newNode;
-                    if(map.containsKey(val)) {
-                        newNode = map.get(val);
-                    }
-                    else {
-                        newNode = new Node(val, new ArrayList());
-                        map.put(val, newNode);
-                    }
-                    
-                    seen.add(n);
-                    queue.add(n);
-                    queue.add(newNode);
-                }
-                
-                if(!curr.neighbors.contains(map.get(val))) {
-                    curr.neighbors.add(map.get(val));
-                }
+            if(!curr.neighbors.contains(newNeighbor)) {
+                curr.neighbors.add(newNeighbor);
+            }
+            
+            if(!visited.contains(neighbor)) {
+                dfs(neighbor, map, visited);
             }
         }
         
-        return head;
+        return curr;
     }
 }
