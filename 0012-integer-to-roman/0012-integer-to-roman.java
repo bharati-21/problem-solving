6f74e057cf101b445b7049394031a8fda2e9b012
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-20 17:59:30 +0530

    Time: 16 ms (31.66%), Space: 47.5 MB (16.28%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -4,31 +4,22 @@ class Solution {
             return "0";
         }
         
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "I");
-        map.put(4, "IV");
-        map.put(5, "V");
-        map.put(9, "IX");
-        map.put(10, "X");
-        map.put(40, "XL");
-        map.put(50, "L");
-        map.put(90, "XC");
-        map.put(100, "C");
-        map.put(400, "CD");
-        map.put(500, "D");
-        map.put(900, "CM");
-        map.put(1000, "M");
-        
         String s = "";
         
+        String[] mapping = new String[] {
+          "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+        };
         int[] places = new int[] {
             1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
         };
         
-        for(int place: places) {
+        for(int i = 0; i<places.length; i++) {
+            int place = places[i];
             int digit = num / place;
+            
             if(digit != 0) {
-                s+= getRomanSymbol(digit, map.get(place));
+                String symbol = mapping[i];
+                s+= getRomanSymbol(digit, symbol);
             }
             
             num = num % place;
