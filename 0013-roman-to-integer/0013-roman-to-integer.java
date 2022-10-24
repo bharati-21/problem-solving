23f738bbcc59ba1f57341299329644005afd6c5c
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-24 23:13:23 +0530

    Time: 23 ms (10.63%), Space: 50.6 MB (6.67%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int romanToInt(String s) {
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        int n = s.length();
+        Map<String, Integer> map = new HashMap();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
+        
+        int integer = map.get(s.substring(n-1));
+        
+        for(int i = n-2; i>=0; i--) {
+            String curr = s.substring(i,i+1);
+            String next = s.substring(i+1, i+2);
+            
+            int currNum = map.get(curr);
+            int nextNum = map.get(next);
+            if(currNum < nextNum) {
+                integer -= currNum;
+            }
+            else {
+                integer += currNum;
+            }
+        }
+        
+        return integer;
+    }
+}
