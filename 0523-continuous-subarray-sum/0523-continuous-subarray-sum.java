3875300e9697202fcc4606e7c7c9aca7c37d8c73
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-26 22:20:16 +0530

    Time: 54 ms (65.92%), Space: 118.6 MB (11.56%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -11,15 +11,42 @@ class Solution {
             prefix[i] = prefix[i-1] + nums[i-1];
         }
         
-        Set<Integer> set = new HashSet();
-        for(int i = 2; i<=n; i++) {
-            int prevMod = prefix[i-2] % k;
-            set.add(prevMod);
-            int mod = prefix[i] % k;
-            if(set.contains(mod)) {
-                return true;
+        HashMap<Integer, Integer> map = new HashMap();
+        map.put(0, -1);
+        int sum = 0;
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+            int remainder = sum % k;
+            
+            if(!map.containsKey(remainder) ) {
+                map.put(remainder, i);
+            }
+            else {
+                int j = map.get(remainder);
+                if(i - j > 1) {
+                    return true;
+                }
             }
         }
+//         Set<Integer> set = new HashSet();
+//         for(int i = 2; i<=n; i++) {
+//             int prevMod = prefix[i-2] % k;
+//             set.add(prevMod);
+//             int mod = prefix[i] % k;
+//             if(set.contains(mod)) {
+//                 return true;
+//             }
+//         }
+//         return false;
+        
+        // for(int i = 0; i<n-1; i++) {
+        //     for(int j = i+2; j<=n; j++) {
+        //         if((prefix[j] - prefix[i]) % k == 0) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        
         return false;
     }
 }
