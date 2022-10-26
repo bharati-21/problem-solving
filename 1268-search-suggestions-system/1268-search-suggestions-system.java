d5c811d801307d2f517ba17ffe6b114fc966107e
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-26 11:57:47 +0530

    Time: 188 ms (24.97%), Space: 82.4 MB (22.21%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -7,16 +7,28 @@ class Solution {
             output.add(i, new ArrayList());
         }
         
+        // O(n*m*n)
+        StringBuilder search = new StringBuilder();
         for(int i = 0; i<n; i++) {
-            String search = searchWord.substring(0, i+1);
+            search.append(searchWord.charAt(i));
             
+            // O(m)
             Set<String> set = new TreeSet<>();
+            
+            // O(m)
             for(String product: products) {
                 if(product.length() < search.length()) {
                     continue;
                 }
-                String sub = product.substring(0, i+1);
-                if(search.equals(sub)) {
+                StringBuilder sub = new StringBuilder();
+                
+                // In the worst case this will run O(n) times
+                for(int j = 0; j<=i; j++) {
+                    sub.append(product.charAt(j));
+                }
+            
+                // O(n)
+                if(sub.toString().equals(search.toString())) {
                     set.add(product);
                 }
             }
