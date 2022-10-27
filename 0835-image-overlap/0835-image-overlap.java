4c291c0667d33f2f4cd9682da1ac479299da9b30
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-27 11:53:35 +0530

    Time: 532 ms (5.45%), Space: 71.8 MB (16.36%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -3,14 +3,51 @@ class Solution {
         int overlaps = 0;
         
         int n = img1.length;
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j<n; j++) {
-                overlaps = Math.max(overlaps, countOverlap(img1, img2, i, j));
-                overlaps = Math.max(overlaps, countOverlap(img2, img1, i, j));
+        
+        List<List<Integer>> listImg1 = getNonZeroPoints(img1);
+        List<List<Integer>> listImg2 = getNonZeroPoints(img2);
+        
+        HashMap<Pair<Integer, Integer>, Integer> linearTrans = new HashMap(); 
+        
+        for(List<Integer> list1: listImg1) {
+            for(List<Integer> list2: listImg2) {
+                int r1 = list1.get(0), c1 = list1.get(1);
+                int r2 = list2.get(0), c2 = list2.get(1);
+                int rowDiff = r2-r1;
+                int colDiff = c2-c1;
+                
+                Pair<Integer, Integer> p = new Pair(rowDiff, colDiff);
+                
+                if(linearTrans.containsKey(p)) {
+                    linearTrans.put(p, linearTrans.get(p) + 1);
+                }
+                else {
+                    linearTrans.put(p, 1);
+                }
+                
+                overlaps = Math.max(overlaps, linearTrans.get(p));
             }
         }
         
         return overlaps;
+    }
+    
+    private List<List<Integer>> getNonZeroPoints(int[][] a) {
+        List<List<Integer>> list = new ArrayList();
+        
+        int n = a.length;
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(a[i][j] == 1) {
+                    List<Integer> points = new ArrayList();
+                    points.add(i);
+                    points.add(j);
+                    list.add(points);
+                }
+            }
+        }
+        
+        return list;
     }
     
     private int countOverlap(int[][] a, int[][] b, int aRowStart, int aColStart) {
@@ -18,10 +55,13 @@ class Solution {
         int sumLeft = 0, sumRight = 0;
         
         int bRow = 0, bCol = 0;
+        System.out.println(aRowStart + " " + aColStart);
         
         for(int i = aRowStart; i<n; i++) {
             bCol = 0;
             for(int j = aColStart; j<n; j++) {
+                System.out.println("a " + i + " " + j + " b " + bRow + " " + bCol);
+                System.out.println("a " + i + " " + bCol + " b " + bRow + " " + j);
                 if(a[i][j] == 1 && a[i][j] == b[bRow][bCol]) {
                     sumLeft++;
                 }
@@ -32,6 +72,16 @@ class Solution {
             }
             bRow++;
         }
+        System.out.println("======\n");
         return Math.max(sumLeft, sumRight);
     }
 }
+
+/*
+00, 00
+01, 01
+02, 02
+10, 10
+11, 11
+
+*/
