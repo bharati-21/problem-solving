b23bfbccd1e30cf782549f8b045597176cef604b
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-30 22:31:03 +0530

    Time: 58 ms (51.50%), Space: 56.3 MB (21.85%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,97 +1,63 @@
+class Block {
+    int row, col, k, step;
+    Block(int row, int col, int k, int step) {
+        this.row = row;
+        this.col = col;
+        this.k = k;
+        this.step = step;
+    }
+}
+
 class Solution {
     public int shortestPath(int[][] grid, int k) {
+        Queue<Block> queue = new LinkedList();
+        int[][] directions = new int[][] {
+            {-1,0}, {1,0}, {0,-1}, {0,1}
+        };
+        
         int m = grid.length, n = grid[0].length;
         
         boolean[][][] visited = new boolean[m][n][k+1];
-        int[][][] memo = new int[m][n][k+1];
-        for(int[][] row1: memo) {
-            for(int[] row2: row1) {
-                Arrays.fill(row2, -1);
+        Block t = new Block(0, 0, k, 0);
+        queue.add(t);
+        visited[0][0][k] = true;
+                
+        while(!queue.isEmpty()) {
+            Block front = queue.poll();
+            int row = front.row;
+            int col = front.col;
+            int removals = front.k;
+            int step = front.step;
+                        
+            if(row == m-1 && col == n-1) {
+                return step;
+            }
+            
+            int curr = grid[row][col];
+            if(curr == 1) {
+                if(removals == 0) {
+                    // you can no longer move down this path
+                    continue;
+                }
+                else {
+                    removals -= 1;
+                }
+            }
+            
+            for(int[] dir: directions) {
+                int nRow = row + dir[0];
+                int nCol = col + dir[1];
+                int newStep = step + 1;
+                if(nRow < 0 || nCol < 0 || nRow >= m || nCol >= n || visited[nRow][nCol][removals]) {
+                    continue;
+                }
+                
+                Block child = new Block(nRow, nCol, removals, newStep);
+                visited[nRow][nCol][removals] = true;
+                queue.add(child);
             }
         }
-        int ans = shortestPathHelper(m-1, n-1, grid, k, visited, memo);
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+        
+        return -1;
     }
-    
-    private int shortestPathHelper(int i, int j, int[][] grid, int k, boolean[][][] visited, int[][][] memo) {
-        int m = grid.length, n = grid[0].length;
-        if(i == 0 && j == 0) {
-            return 0;
-        }
-        if(visited[i][j][k]) {
-            return Integer.MAX_VALUE;
-        }
-        
-        if(memo[i][j][k] != -1) {
-            return memo[i][j][k];
-        }
-        
-        // if(k >= i + j) return memo[i][j][k] = i+j;
-        
-        visited[i][j][k] = true;
-        int left = Integer.MAX_VALUE, 
-            right = Integer.MAX_VALUE, 
-            down = Integer.MAX_VALUE, 
-            top = Integer.MAX_VALUE;
-        
-        int curr = grid[i][j];
-        if(curr == 1 && k == 0) {
-            return memo[i][j][k] = Integer.MAX_VALUE;
-        }
-        
-        int l = j-1;
-        int r = j+1;
-        int t = i-1;
-        int d = i+1;
-        
-        // I can move in either of the 4 directions
-        // Need to check the boundaries for them
-        // Another catch is what if I have an obstacle
-        // To eliminate or not eliminate is another option
-        // If I have an obstacle than I have 2*4 = 8 choices
-        // else I have 4 choices
-        
-        if(l >= 0) {
-            if(curr == 0)
-                left = shortestPathHelper(i, l, grid, k, visited, memo);
-            if(curr == 1) {
-                // remove this obstacle and also check
-                left = Math.min(left, shortestPathHelper(i, l, grid, k-1, visited, memo));
-            }
-        }
-        if(r < n) {
-            if(curr == 0)
-                right = shortestPathHelper(i, r, grid, k, visited, memo);
-            if(curr == 1 ) {
-                // remove this obstacle and also check
-                right = Math.min(right, shortestPathHelper(i, r, grid, k-1, visited, memo));
-            }
-        }
-        if(t >= 0) {
-            if(curr == 0)
-                top = shortestPathHelper(t, j, grid, k, visited, memo);
-            if(curr == 1) {
-                // remove this obstacle and also check
-                top = Math.min(top, shortestPathHelper(t, j, grid, k-1, visited, memo));
-            }
-        }
-        if(d < m) {
-            if(curr == 0)
-                down = shortestPathHelper(d, j, grid, k, visited, memo);
-            if(curr == 1) {
-                // remove this obstacle and also check
-                down = Math.min(down, shortestPathHelper(d, j, grid, k-1, visited, memo));
-            }
-        }
-            
-        int iMin = Math.min(top, down);
-        int jMin = Math.min(right, left);
-        int min = Math.min(iMin, jMin);
-        if(min != Integer.MAX_VALUE) {
-            min += 1;
-        }
-            
-        visited[i][j][k] = false;
-        return memo[i][j][k] = min;
-    } 
 }
