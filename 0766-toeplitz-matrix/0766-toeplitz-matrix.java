800afb4a23f3b450755e64e952bec4493a9f6a7b
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-31 07:42:39 +0530

    Time: 2 ms (42.33%), Space: 45.9 MB (37.47%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        Map<Integer, Integer> map = new HashMap();
-        
         int m = matrix.length, n = matrix[0].length;
         
-        for(int i = 0; i<m; i++) {
+        for(int i = 0; i<m-1; i++) {
             for(int j = 0; j<n; j++) {
                 int curr = matrix[i][j];
-                int diagonalNum = i-j;
-                
-                if(map.containsKey(diagonalNum)) {
-                    int diagVal = map.get(diagonalNum);
-                    if(diagVal != curr) {
+                if(j+1 < n) {
+                    int prev = matrix[i+1][j+1];
+                    if(curr != prev) {
                         return false;
                     }
                 }
-                else {
-                    map.put(diagonalNum, curr);
-                }
             }
         }
+        
         return true;
     }
 }
