7108b5b538b9671381d61e0c70eea0e1a76dbc75
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-01 09:09:45 +0530

    Time: 63 ms (34.17%), Space: 42.9 MB (43.76%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        return isMatchHelper(s, p, 0, 0);
+    }
+    
+    private boolean isMatchHelper(String s, String p, int i, int j) {
+        int m = s.length(), n = p.length();
+        if(i >= m && j >= n) {
+            return true;
+        }
+        
+        if(j >= n) {
+            return false;
+        }
+        
+        boolean matches = false;
+        if(i < m) {
+            char sCh = s.charAt(i);
+            char pCh = p.charAt(j);
+            if(sCh == pCh || pCh == '.') {
+                matches = true;
+            }
+        }
+            
+        int next = j+1;
+        if(next < n && p.charAt(next) == '*') {
+            return (
+                    isMatchHelper(s, p, i, j+2) ||
+                        (matches && isMatchHelper(s, p, i+1, j))
+                 );
+        }
+            
+        if(matches) {
+            return isMatchHelper(s, p, i+1, j+1);
+        }
+        
+        return matches;
+    }
+}
