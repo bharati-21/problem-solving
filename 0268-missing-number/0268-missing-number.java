3fc7c8f31ed8b526ca9b42a38b3a9b0cd787ffd6
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-06 10:46:28 +0530

    Time: 1 ms (88.89%), Space: 51.3 MB (29.31%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int n = nums.length;
+        int i = 0;
+        
+        int xor = 0;
+        for(; i<n; i++) {
+            int num = nums[i];
+            xor = xor ^ i ^ num;
+        }
+        
+        return xor ^ i;
+    }
+}
+
+/*
+0^3^1^0^2^1^3
+*/
+
+/*
+Naive: Run outer loop from 0 to n, check for every value num, if it exists in array or not
+Sort: Sort the array and if the difference between 2 nums > 1 then return the current number + 1
+Set: Add all numbers to it. Iterate again from 0 to n, if a number is not in set, return that num
+Count sort: place number i at ith index. At the end if every number is in it's own position return n. Else return the number that is not in it's correct position
+Return Sum of n natural numbers - return the sum of elements in array 
+XOR
+*/
