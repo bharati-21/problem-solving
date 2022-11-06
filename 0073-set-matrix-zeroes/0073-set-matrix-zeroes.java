7490c6bbc45a836e2ad29e9602a8ad5c50b12422
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-06 19:10:53 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (97.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -6,14 +6,14 @@ class Solution {
         
         int m = matrix.length, n = matrix[0].length;
         
-        boolean firstColZero = false;
-        for(int i = 0; i<m; i++) {
-            
-            if(matrix[i][0] == 0) {
-                firstColZero = true;
-            }
-            
-            for(int j = 1; j<n; j++) {
+        boolean firstRowZero = false;
+        
+        for(int j = 0; j<n; j++) {
+            if(matrix[0][j] == 0) firstRowZero = true;
+        }
+        
+        for(int i = 1; i<m; i++) {
+            for(int j = 0; j<n; j++) {
                 int cell = matrix[i][j];
                 if(cell == 0) {
                     matrix[0][j] = 0;
@@ -31,14 +31,14 @@ class Solution {
         }
         
         if(matrix[0][0] == 0) {
-            for(int j = 0; j<n; j++) {
-                matrix[0][j] = 0;
+            for(int i = 0; i<m; i++) {
+                matrix[i][0] = 0;
             }
         }
         
-        if(firstColZero) {
-            for(int i = 0; i<m; i++) {
-                matrix[i][0] = 0;
+        if(firstRowZero) {
+            for(int j = 0; j<n; j++) {
+                matrix[0][j] = 0;
             }
         }
     }
