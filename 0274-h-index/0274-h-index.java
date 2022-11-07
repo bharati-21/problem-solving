575061325e5926d4839b144ccf06469667ad43d9
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-07 11:02:52 +0530

    Time: 3 ms (53.35%), Space: 41.4 MB (78.66%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -3,6 +3,9 @@ class Solution {
         Arrays.sort(citations);
         
         int n = citations.length;
+        // If the citation at i is h, then we need h papers more that satisfy this
+        // That h papers can be found by (n-i) (number of papers to the right of i)
+        // So if h >= (n-i), that means we have at least h papers
         for(int i = 0; i<n; i++) {
             int citation = citations[i];
             if(citation >= (n-i)) {
@@ -21,6 +24,10 @@ Number of papers having 2 citations:
 Number of papers having 3 citations: 1
 Number of papers having 4 citations: 0
 Number of papers having 5 citations: 
+0 1 3 5 6
+0 >= (5-0)
+1 >= (5-1)
+3 >= (5-2)
 1 1 3
 1 1 2 4
 */
