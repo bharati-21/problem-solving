a94a289bb6d70624dfecf8e72c1b85f9b96e9444
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-08 10:45:14 +0530

    Time: 404 ms (13.53%), Space: 240.8 MB (14.73%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if(n == 0) {
+            return false;
+        }
+        
+        List<List<Integer>> adj = getAdjacencyList(edges, n);
+        return dfs(adj, source, destination, new HashSet());
+    }
+    
+    // adj = [[1,2],[0,2],[0,1]]
+    // visited = [0]
+    // 
+    
+    private boolean dfs(List<List<Integer>> adj, int source, int dest, Set<Integer> visited) {
+        visited.add(source);
+        if(source == dest) {
+            return true;
+        }
+        
+        for(int node: adj.get(source)) {
+            if(!visited.contains(node)) {
+                if(dfs(adj, node, dest, visited)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private List<List<Integer>> getAdjacencyList(int[][] edges, int n) {
+        List<List<Integer>> adj = new ArrayList();
+        for(int i = 0; i<n; i++) {
+            adj.add(i, new ArrayList());
+        }
+        
+        for(int[] edge: edges) {
+            int u = edge[0], v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        
+        return adj;
+    }
+}
