32cd013c3651727ad86b19b9656844904d02ec3f
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-14 10:12:11 +0530

    Time: 27 ms (81.37%), Space: 42.6 MB (88.52%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,35 +1,68 @@
+class UnionFind {
+    int[] parent, rank;
+    int n;
+    int connectedComponents;
+    
+    UnionFind(int n) {
+        this.n = n;
+        parent = new int[n];
+        rank = new int[n];
+        connectedComponents = n;
+        
+        for(int i = 0; i<n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+    
+    private int find(int x) {
+        if(x == parent[x]) return x;
+        
+        return parent[x] = find(parent[x]);
+    }
+    
+    public void union(int x, int y) {
+        int parX = find(x);
+        int parY = find(y);
+        
+        if(parX == parY) {
+            return;
+        }
+        
+        if(rank[parX] < rank[parY]) {
+            parent[parX] = parent[parY];
+            rank[parY]++;
+        }
+        else {
+            parent[parY] = parent[parX];
+            rank[parX]++;
+        }
+        
+        connectedComponents--;
+    }
+    
+    public int getNumConnectedComponents() {
+        return connectedComponents;
+    }
+}
+
 class Solution {
     public int removeStones(int[][] stones) {
         if(stones == null || stones.length == 0) return 0;
         
         int n = stones.length;
-        Set<int[]> visited = new HashSet();
-        int islands = 0;
+        UnionFind uf = new UnionFind(n);
         
         for(int i = 0; i<n; i++) {
-            int[] stone = stones[i];
-            if(!visited.contains(stone)) {
-                islands++;
-                dfs(i, stones, visited);
-            }
-        }
-        
-        return n - islands;
-    }
-    
-    private void dfs(int row, int[][] stones, Set<int[]> visited) {
-        int[] stone = stones[row];
-        int n = stones.length;
-        
-        visited.add(stone);
-        
-        for(int i = 0; i<n; i++) {
-            int[] s = stones[i];
-            if(!visited.contains(s)) {
-                if(stone[0] == s[0] || stone[1] == s[1]) {
-                    dfs(i, stones, visited);
+            int[] s1 = stones[i];
+            for(int j = i+1; j<n; j++) {
+                int[] s2 = stones[j];
+                if(s1[0] == s2[0] || s1[1] == s2[1]) {
+                    uf.union(i,j);
                 }
             }
         }
+        
+        return n - uf.getNumConnectedComponents();
     }
 }
