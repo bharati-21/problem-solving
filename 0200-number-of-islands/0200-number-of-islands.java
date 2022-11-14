19a787ecba513197539573bc9ef0f5f075334f2f
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-14 23:18:30 +0530

    Time: 22 ms (11.83%), Space: 56.6 MB (69.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -15,7 +15,7 @@ class Solution {
                 boolean seen = visited[r][c];
                 
                 if(cell == '1' && !seen) {
-                    bfs(r, c, m, n, grid, visited);
+                    bfs(r, c, grid, visited);
                     islands++;
                 }
             }
@@ -24,11 +24,11 @@ class Solution {
         return islands;
     }
     
-    private void bfs(int r, int c, int m, int n, char[][] grid, boolean[][] visited) {
+    private void bfs(int row, int col, char[][] grid, boolean[][] visited) {
         Queue<int []> queue = new LinkedList();
         
-        visited[r][c] = true;
-        queue.add(new int[] {r,c});
+        visited[row][col] = true;
+        queue.add(new int[] {row, col});
         
         int[][] directions = new int[][] {
             {-1,0}, {1,0}, {0,-1}, {0,1}
@@ -36,20 +36,27 @@ class Solution {
         
         while(!queue.isEmpty()) {
             int[] cell = queue.poll();
+            int currRow = cell[0];
+            int currCol = cell[1];
             
             for(int[] dir: directions) {
-                int nextRow = dir[0] + cell[0];
-                int nextCol = dir[1] + cell[1];
-                if(isInvalidCell(nextRow, nextCol, m, n, grid, visited)) {
-                    visited[nextRow][nextCol] = true;
-                    queue.add(new int[] { nextRow, nextCol });
+                int nextRow = dir[0] + currRow;
+                int nextCol = dir[1] + currCol;
+                
+                if(!isValidCell(nextRow, nextCol, grid, visited)) {
+                    continue;
                 }
+                
+                visited[nextRow][nextCol] = true;
+                queue.add(new int[] { nextRow, nextCol });
             }
         }
     }
     
-    private boolean isInvalidCell(int r, int c, int m, int n, char[][] grid, boolean[][] visited) {
-        if(r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == '0' || visited[r][c]) {
+    private boolean isValidCell(int row, int col, char[][] grid, boolean[][] visited) {
+        int m = grid.length, n = grid[0].length;
+        
+        if(row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == '0' || visited[row][col]) {
             return false;
         }
         
