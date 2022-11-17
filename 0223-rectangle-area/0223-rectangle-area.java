335690e526a4198259605fcdb7ef5d698d74789c
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-17 22:34:49 +0530

    Time: 4 ms (81.38%), Space: 43.4 MB (24.31%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.java
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -3,7 +3,7 @@ class Solution {
         int area1 = (ax2 - ax1) * (ay2 - ay1);
         int area2 = (bx2 - bx1) * (by2 - by1);
         
-        if(ax2 < bx1 || ax1 > bx2 || ay2 < by1 || ay1 > by2) {
+        if(ax2 <= bx1 || ax1 >= bx2 || ay2 <= by1 || ay1 >= by2) {
             return area1 + area2;
         }
         
