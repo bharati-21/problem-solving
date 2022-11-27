702d09f3ca3e59823b7f7dcfe8259cac8dc5a943
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-27 13:31:25 +0530

    Time: 0 ms (100.00%), Space: 42 MB (44.61%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -4,21 +4,22 @@ class Solution {
         if(n < 3) return 0;
         
         int numSlices = 0;
+        int[] dp = new int[n];
         
-        for(int i = 0; i<n-2; i++) {
-            int first = nums[i];
-            int second = nums[i+1];
-            int diff = (second - first);
-            for(int j = i+2; j<n; j++) {
-                int third = nums[j];
-                if(nums[j] - second == diff) {
-                    second = nums[j];
-                    numSlices++;
-                }
-                else {
-                    break;
-                }
+        for(int i = 2; i<n; i++) {
+            int first = nums[i-1] - nums[i-2];
+            int second = nums[i] - nums[i-1];
+            
+            if(first == second) {
+                dp[i] = dp[i-1] + 1;
             }
+            else {
+                dp[i] = 0;
+            }
+        }
+        
+        for(int count: dp) {
+            numSlices += count;
         }
         
         return numSlices;
@@ -26,5 +27,4 @@ class Solution {
 }
 
 /*
-Naive solution: O(n^3) where we check for all combinations of i,j,k 
 */
