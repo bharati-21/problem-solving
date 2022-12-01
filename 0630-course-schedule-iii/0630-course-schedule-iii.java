2a83dcf0dc621a9c8000554083822327d4942616
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-12-01 11:53:21 +0530

    Time: 945 ms (5.18%), Space: 69 MB (63.29%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+        int n = courses.length;
+        // Greedy to take courses that end faster
+        Arrays.sort(courses, (a,b) -> a[1]-b[1]);
+        int time = 0;
+        int count = 0;
+        
+        for(int i = 0; i<n; i++) {
+            int[] course = courses[i];
+            int duration = course[0];
+            int lastDay = course[1];
+            
+            if(time + duration <= lastDay) {
+                time += duration;
+                count++;
+            }
+            else {
+                int maxDuration = i;
+                for(int j = 0; j<i; j++) {
+                    if(courses[maxDuration][0] < courses[j][0]) {
+                        maxDuration = j;
+                    }
+                }
+                    
+                // If a previous course was taken that was of a very long duration
+                // replace it with the current course
+                if(duration < courses[maxDuration][0]) {
+                    // subtract the extra time taken by the maxDuration course
+                    // as we have replaced it with a course of lesser duration
+                    int saveTime = courses[i][0] - courses[maxDuration][0];
+                    time += saveTime;
+                }
+                courses[maxDuration][0] = 0;
+            }
+        }
+        
+        return count;
+    }
+}
