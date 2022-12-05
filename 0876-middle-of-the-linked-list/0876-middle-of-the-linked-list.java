da01e47d8dde841d6b395b1ca9ca2ea7470836ed
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-12-05 09:24:01 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (93.84%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head == null) return head;
+        
+        ListNode slowPtr = head, fastPtr = head;
+        while(fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+        
+        return slowPtr;
+    }
+}
+
+/*
+Find the length and then go till the node length/2
+or
+use two pointers. when the fast pointer reaches the end, the slow pointer reaches the middle
+*/
