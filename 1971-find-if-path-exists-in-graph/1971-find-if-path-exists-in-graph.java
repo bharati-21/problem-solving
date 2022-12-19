8a3b0e87a9700e3f683e93cba1263bdc8b415d9f
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-12-19 13:45:02 +0530

    Time: 306 ms (25.26%), Space: 161.3 MB (71.77%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,58 +1,39 @@
-class Union {
-    int n;
-    int[] parent;
-    int[] rank;
-    
-    Union(int n) {
-        this.n = n;
-        this.rank = new int[n];
-        this.parent = new int[n];
-        
-        for(int i = 0; i<n; i++) {
-            parent[i] = i;
-        }
-    }
-    
-    public int find(int u) {
-        if(u == parent[u]) {
-            return u;
-        }
-        return parent[u] = find(parent[u]);
-    }
-    
-    public void union(int u, int v) {
-        int parU = find(u);
-        int parV = find(v);
-        
-        if(parU != parV) {
-            if(rank[u] >= rank[v]) {
-                parent[parV] = parU;
-                rank[u]++;
-            }
-            else {
-                parent[parU] = parV;
-                rank[v]++;
-            }
-        }
-    }
-    
-    public boolean areConnected(int u, int v){
-        return find(u) == find(v);
-    }
-}
-
 class Solution {
+    private List<List<Integer>> adj;
+    private Set<Integer> visited;
+    
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Union union = new Union(n);
-        
-        
-        for(int i = 0; i<edges.length; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            
-            union.union(u, v);
+        adj = new ArrayList();
+        for(int i = 0; i<n; i++) {
+            adj.add(new ArrayList());
         }
-                
-        return union.areConnected(source, destination);
+        generateAdjList(edges);
+        visited = new HashSet();
+        return dfs(source, destination);
+    }
+    
+    private void generateAdjList(int[][] edges) {
+        for(int[] edge: edges) {
+            int u = edge[0];
+            int v = edge[1];
+            
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+    }
+    
+    private boolean dfs(int source, int destination) {
+        visited.add(source);
+        if(source == destination) return true;
+        
+        boolean hasPath = false;
+        
+        for(int node: adj.get(source)) {
+            if(!visited.contains(node)) {
+                hasPath |= dfs(node, destination);
+            }
+        }
+        
+        return hasPath;
     }
 }
