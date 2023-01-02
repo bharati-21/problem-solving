10ca1896191277257dfe7792f3cd3d33e6607f70
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-01-02 13:22:58 +0530

    Time: 12 ms (79.77%), Space: 43.4 MB (71.43%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int max = Integer.MAX_VALUE;
+        int[][] distance = new int[n][n];
+        
+        for(int[] row: distance) {
+            Arrays.fill(row, max);
+        }
+        
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(i == j) {
+                    distance[i][j] = 0;
+                    continue;
+                }
+            }
+        }
+        
+        for(int[] edge: edges) {
+            int u = edge[0], v = edge[1], weight = edge[2];
+            distance[u][v] = weight;
+            distance[v][u] = weight;
+        }
+        
+        for(int k = 0; k<n; k++) {
+            for(int i = 0; i<n; i++) {
+                for(int j = 0; j<n; j++) {
+                    if(distance[i][k] == max || distance[k][j] == max) continue;
+                    if(distance[i][j] > distance[i][k] + distance[k][j]) {
+                        distance[i][j] = distance[i][k] + distance[k][j];
+                    }
+                }
+            }
+        }
+        
+        int city = 0, count = max;
+        
+        for(int i = 0; i<n; i++) {
+            int currCount = 0;
+            for(int j = 0; j<n; j++) {
+                if(distance[i][j] <= distanceThreshold) {
+                    currCount++;
+                }
+            }
+            
+            if(count > currCount) {
+                count = currCount;
+                city = i;
+            }
+            else if(count == currCount && city < i) {
+                city = i;
+            }
+        }
+        
+        return city;
+    }
+}
