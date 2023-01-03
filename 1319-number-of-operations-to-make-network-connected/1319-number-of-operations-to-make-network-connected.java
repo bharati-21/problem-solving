270779a73b584d76e114b019501ac4f7cf4aca17
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-01-03 08:33:35 +0530

    Time: 48 ms (21.33%), Space: 75 MB (23.87%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,69 +1,38 @@
-class UnionFind {
-    int n, components;
-    int[] parent, rank;
-    int extraCables;
-    
-    UnionFind(int n) {
-        this.n = n;
-        components = n;
-        parent = new int[n];
-        rank = new int[n];
-        extraCables = 0;
-        
-        for(int i = 0; i<n; i++) {
-            parent[i] = i;
-            rank[i] = 0;
-        }
-    }
-    
-    int find(int u) {
-        if(parent[u] == u) {
-            return u;
-        }
-        
-        return parent[u] = find(parent[u]);
-    }
-    
-    void union(int u, int v) {
-        int parU = find(u), parV = find(v);
-        
-        if(parU == parV) {
-            extraCables++;
-            return;
-        }
-        
-        if(rank[parU] < rank[parV]) {
-            parent[parU] = parV;
-        }
-        else {
-            parent[parV] = parU;
-            if(rank[parV] == rank[parU]) {
-                rank[parU]++;
-            }
-        }
-        components--;
-    }
-}
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        UnionFind uf = new UnionFind(n);
+        // to connect n components we need at least n-1 edges
+        if(connections.length < (n-1)) return -1;    
+        
+        Set<Integer> visited = new HashSet();
+        List<List<Integer>> adj = new ArrayList();
+        
+        for(int i = 0; i<n; i++) adj.add(new ArrayList());
         
         for(int[] connection: connections) {
             int u = connection[0], v = connection[1];
             
-            uf.union(u, v);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
         
-        int components = uf.components;
-        int extraCables = uf.extraCables;
-                
-        if((components-1) <= extraCables) {
-            return components-1;
+        int components = 0;
+        for(int i = 0; i<n; i++) {
+            if(!visited.contains(i)) {
+                traverseNodes(i, adj, visited);
+                components++;
+            }
         }
         
-        return -1;
+        return components-1;
+    }
+    
+    private void traverseNodes(int node, List<List<Integer>> adj, Set<Integer> visited) {
+        visited.add(node);
+        
+        for(int next: adj.get(node)) {
+            if(!visited.contains(next)) {
+                traverseNodes(next, adj, visited);
+            }
+        }
     }
 }
-
-// if I have n disconnected components
-// to connect them I need at least n-1 cables to make them connected
