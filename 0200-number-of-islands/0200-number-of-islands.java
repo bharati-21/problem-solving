a0e5b86025b4159e00be021bcbf6c124505a36df
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-01-04 12:52:08 +0530

    Time: 51 ms (5.11%), Space: 57.2 MB (43.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,65 +1,90 @@
-class Solution {
-    public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0) {
-            return 0;
-        }
+class UnionFind {
+    int[] parent, rank;
+    int m, n;
+    int count;
+    
+    UnionFind(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
         
-        int m = grid.length, n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        
-        int islands = 0;
-        
-        for(int r = 0; r<m; r++) {
-            for(int c = 0; c<n; c++) {
-                int cell = grid[r][c];
-                boolean seen = visited[r][c];
+        parent = new int[m*n];
+        Arrays.fill(parent, -1);
+        rank = new int[m*n];
+        count = 0;
                 
-                if(cell == '1' && !seen) {
-                    bfs(r, c, grid, visited);
-                    islands++;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1') {
+                    count++;
+                    int id = (i * n) + j;
+                    parent[id] = id;
+                    rank[id] = 1;
                 }
             }
         }
-        
-        return islands;
     }
     
-    private void bfs(int row, int col, char[][] grid, boolean[][] visited) {
-        Queue<int []> queue = new LinkedList();
+    int find(int u) {
+        if(parent[u] == u){
+            return u;
+        }
         
-        visited[row][col] = true;
-        queue.add(new int[] {row, col});
+        return parent[u] = find(parent[u]);
+    }
+    
+    void union(int u, int v) {
+        int parU = find(u);
+        int parV = find(v);
         
-        int[][] directions = new int[][] {
+        if(parU == parV) return;
+        
+        if(rank[parU] >= rank[parV]) {
+            parent[parV] = parU;
+            
+            if(rank[parU] == rank[parV]) {
+                rank[parU]++;
+            }
+        }
+        else {
+            parent[parU] = parV;
+        }
+        count--;
+    }
+}
+
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int rows = grid.length, cols = grid[0].length;
+        
+        UnionFind uf = new UnionFind(grid);
+        
+        int[][] dir = new int[][] {
             {-1,0}, {1,0}, {0,-1}, {0,1}
         };
         
-        while(!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            int currRow = cell[0];
-            int currCol = cell[1];
-            
-            for(int[] dir: directions) {
-                int nextRow = dir[0] + currRow;
-                int nextCol = dir[1] + currCol;
-                
-                if(!isValidCell(nextRow, nextCol, grid, visited)) {
-                    continue;
+        for(int row = 0; row < rows; row++) {
+            for(int col = 0; col < cols; col++) {
+                if(grid[row][col] == '1') {
+                    
+                    int cell = (row * cols) + col;
+                    
+                    for(int[] d: dir) {
+                        int nextRow = d[0] + row;
+                        int nextCol = d[1] + col;
+                        
+                        if(nextRow >= rows || nextRow < 0 || nextCol >= cols || nextCol < 0 ||
+                          grid[nextRow][nextCol] == '0') {
+                            continue;
+                        }
+                        
+                        int nextCell = (nextRow * cols) + nextCol;
+                        uf.union(cell, nextCell);
+                    }
                 }
-                
-                visited[nextRow][nextCol] = true;
-                queue.add(new int[] { nextRow, nextCol });
             }
         }
-    }
-    
-    private boolean isValidCell(int row, int col, char[][] grid, boolean[][] visited) {
-        int m = grid.length, n = grid[0].length;
         
-        if(row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == '0' || visited[row][col]) {
-            return false;
-        }
-        
-        return true;
+        return uf.count;
     }
 }
