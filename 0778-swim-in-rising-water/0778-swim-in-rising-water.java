1d5aa101ffaf8d904b388e8f8ec71b7c7502ee42
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-01-04 20:44:52 +0530

    Time: 15 ms (65.96%), Space: 42.2 MB (83.12%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -2,36 +2,55 @@ class Solution {
     public int swimInWater(int[][] grid) {
         int rows = grid.length, cols = grid[0].length;
         
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b) -> a[2] - b[2]);
-        pq.add(new int[] { 0, 0, grid[0][0] });
+        TreeSet<int[]> set = new TreeSet<int[]>((a,b) -> {
+            if(a[2] == b[2]) {
+                if(a[1] == b[1]) return a[0] - b[0];
+                return a[1] - b[1];
+            }
+            return a[2] - b[2];
+        });
+        set.add(new int[] { 0, 0, grid[0][0] });
         
-        boolean[][] visited = new boolean[rows][cols];
-        visited[0][0] = true;
+        int[][] distance = new int[rows][cols];
+        for(int[] row: distance) {
+            Arrays.fill(row, rows*cols);
+        }
+        distance[0][0] = grid[0][0];
         
         int[][] directions = new int[][] {
             {-1,0}, {1,0}, {0,-1}, {0,1}
         };
         
-        while(!pq.isEmpty()) {
-            int[] curr = pq.poll();
+        while(!set.isEmpty()) {
+            int[] curr = set.first();
+            set.remove(curr);
+                        
             int row = curr[0], col = curr[1], weight = curr[2];
-            visited[row][col] = true;
             
             if(row == rows-1 && col == cols-1) {
-                return weight;
+                return distance[row][col];
             }
             
             for(int[] dir: directions) {
                 int nRow = dir[0] + row, nCol = dir[1] + col;
                 
-                if(nRow < 0 || nCol < 0 || nRow >= rows || nCol >= cols ||
-                  visited[nRow][nCol]) {
+                if(nRow < 0 || nCol < 0 || nRow >= rows || nCol >= cols) {
                     continue;
                 }
                 
-                int nWeight = Math.max(grid[nRow][nCol], weight);
+                int nWeight = grid[nRow][nCol];
+                nWeight = Math.max(weight, nWeight);
                 
-                pq.add(new int[] {nRow, nCol, nWeight });
+                if(nWeight < distance[nRow][nCol]) {
+                    int[] next = new int[] {
+                        nRow, nCol, distance[nRow][nCol]
+                    };
+                    
+                    set.remove(next);
+                    distance[nRow][nCol] = nWeight;
+                    next[2] = nWeight;
+                    set.add(next);
+                }
             }
         }
         
