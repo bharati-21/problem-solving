5a1ee120d834e7398b9f95ae996d889d0e62b7c9
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-01-05 21:41:12 +0530

    Time: 70 ms (76.63%), Space: 79.5 MB (77.71%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,44 +1,31 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        int n = points.length;
-        Arrays.sort(points, (a,b) -> a[0]-b[0]);
+        if(points == null || points.length == 0) {
+            return 0;
+        }
         
-        List<int[]> combinedPoints = new ArrayList();
-        int[] prevPoint = points[0];
-        for(int i = 1; i<n; i++) {
-            int[] currPoint = points[i];
-            // check if points are intersecting
-            if(areNotIntersecting(currPoint, prevPoint)) {
-                combinedPoints.add(prevPoint);
-                prevPoint = currPoint;
+        // sort by start time
+        Arrays.sort(points, (a,b) -> a[0] - b[0]);
+        
+        int[] prev = points[0];
+        int numArrows = 0;
+        
+        for(int i = 1; i<points.length; i++) {
+            int[] curr = points[i]; 
+            if(doesNotOverlap(prev, curr)) {
+                numArrows++;
+                prev = curr;
             }
             else {
-                prevPoint[0] = Math.max(prevPoint[0], currPoint[0]);
-                prevPoint[1] = Math.min(prevPoint[1], currPoint[1]);
+                prev[0] = Math.max(prev[0], curr[0]);
+                prev[1] = Math.min(prev[1], curr[1]);
             }
         }
-        combinedPoints.add(prevPoint);
-        return combinedPoints.size();
+        
+        return (numArrows+1);
     }
     
-    private boolean areNotIntersecting(int[] p1, int[] p2) {
-        return (p1[0] > p2[1] || p1[1] < p2[0]);
+    private boolean doesNotOverlap(int[] prev, int[] curr) {
+        return (prev[1] < curr[0] || prev[0] > curr[1]);
     }
 }
-
-/*
-
-[1,6], [2,8], [7,12], [10,16]
-
-given point p1 and p2
-we need to find if p1 and p2 have an intersection
-
-[[1,4],[2,3],[3,4],[4,5]]
-
-[1,4]
-
-
-[[1,2],[2,3],[3,4],[4,5]]
-
-
-*/
