5e2b63788750d8cc99cdad0d580bb861718365e5
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-01-07 00:24:49 +0530

    Time: 0 ms (100.00%), Space: 41 MB (24.67%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,13 +1,24 @@
 class Solution {
+    int[] dp;
     public int numTrees(int n) {
+        dp = new int[n+1];
+        
+        Arrays.fill(dp, -1);
+        
+        return numTreesHelper(n);
+    }
+    
+    private int numTreesHelper(int n) {
         if(n <= 1) return 1;
         if(n == 2) return 2;
         
+        if(dp[n] != -1) return dp[n];
+            
         int ans = 0;
         for(int i = 1; i<=n; i++) {
-            ans += numTrees(i-1) * numTrees(n-i);
+            ans += numTreesHelper(i-1) * numTreesHelper(n-i);
         }
         
-        return ans;
+        return dp[n] = ans;
     }
 }
