977bfd7f4c0b55d9db06205b929fc02c301e5458
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-01-08 12:03:31 +0530

    Time: 35 ms (69.90%), Space: 42.2 MB (82.37%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -4,36 +4,66 @@ class Solution {
         int n = points.length;
         if(n == 1) return 1;
         
+        
         // 1st point
         for(int i = 0; i<n; i++) {
+            Map<List, Integer> gradientCount = new HashMap();
             int[] first = points[i];
-            double x1 = first[0], y1 = first[1];
+            int x1 = first[0], y1 = first[1];
             // 2nd point
             for(int j = 0; j<n; j++) {
                 if(j == i) continue;
                 int[] second = points[j];
-                double x2 = second[0], y2 = second[1];
+                int x2 = second[0], y2 = second[1];
                 
-                double gradient = (y2-y1)/(x2-x1);
+                int dx = x2 - x1;
+                int dy = y2 - y1;
                 
-                // other points with first and second
-                int currCount = 2;
-                for(int k = 0; k<n; k++) {
-                    if(k == i || k == j) continue;
-                    
-                    int[] third = points[k];
-                    double x3 = third[0], y3 = third[1];
-                    
-                    double currGradient = (y3-y2)/(x3-x2);
-                    if(currGradient == gradient) {
-                        currCount++;
-                    }
+                List<Integer> gradient = new ArrayList();
+                
+                if(dx == 0) {
+                    gradient.add(1);
+                    gradient.add(0);
+                }
+                if(dy == 0) {
+                    gradient.add(0);
+                    gradient.add(1);
+                }
+                else {
+                    int gcd = getGCD(dy, dx);
+                    gradient.add(dy/gcd);
+                    gradient.add(dx/gcd);
                 }
                 
-                count = Math.max(count, currCount);
+                gradientCount.put(gradient, 
+                    gradientCount.getOrDefault(gradient,0)+1);
+            }
+            
+            for(Map.Entry<List, Integer> entry: 
+                gradientCount.entrySet()) {
+                int slopeCount = entry.getValue();
+
+                count = Math.max(slopeCount+1, count);
             }
         }
         
+        
+        
         return count;
     }
+    
+    private int getGCD(int num1, int num2) {
+        if(num2 == 0) {
+            return num1;
+        }
+        
+        return getGCD(num2, num1%num2);
+    }
 }
+
+/*
+gcd(36,24)
+gcd(24, 36%24) => gcd(24,12)
+gcd(12,24%12) => gcd(12, 0)
+
+*/
