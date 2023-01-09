8686bbcbbea1973ab27f94459c4fb1267b67c643
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-01-09 22:37:22 +0530

    Time: 1 ms (91.27%), Space: 39.7 MB (77.01%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,32 +1,17 @@
 class Solution {
-    private int n;
-    private int[] cache;
     public int combinationSum4(int[] nums, int target) {
-        n = nums.length;
-        cache = new int[target+1];
-        Arrays.fill(cache, -1);
-        return combinationSumHelper(nums, target);
-    }
-    
-    private int combinationSumHelper(int[] nums, int target) {
-        if(target < 0) {
-            return 0;
+        int n = nums.length;
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+
+        for(int j = 1; j<=target; j++) {
+            for(int i = 0; i<n; i++) {
+                int num = nums[i];
+                if(j - num >= 0) {
+                    dp[j] += dp[j - num];
+                }
+            }
         }
-        
-        if(target == 0) {
-            return 1;
-        }
-        
-        if(cache[target] != -1) {
-            return cache[target];
-        }
-        
-        int ways = 0;
-        
-        for(int i = 0; i<n; i++) {
-            ways += combinationSumHelper(nums, target-nums[i]);
-        }
-        
-        return cache[target] = ways;
+        return dp[target];
     }
 }
