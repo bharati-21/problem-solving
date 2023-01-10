01e8afdd9cf62a0a517a785e6a220aa3966f0059
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-01-10 09:46:59 +0530

    Time: 72 ms (41.77%), Space: 42 MB (72.00%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,25 +1,29 @@
 class Solution {
+    private int[] cache;
     public int numSquares(int n) {
-        if(n <= 0) return 0;
-        
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        
-        for(int i = 1; i<=n; i++) {
-            int answer = Integer.MAX_VALUE;
-            
-            for(int j = 1; j<=Math.sqrt(i); j++) {
-                int nextNum = i - (j * j);
-                if(nextNum < 0) {
-                    continue;
-                }
-                
-                int nextAnswer = dp[nextNum];
-                answer = Math.min(answer, nextAnswer);
-            }
-            
-            dp[i] = answer + 1;
+        cache = new int[n+1];
+        Arrays.fill(cache, -1);
+        return numSquaresHelper(n);
+    }
+    
+    private int numSquaresHelper(int n) {
+        if(n == 0) {
+            return 0;
         }
-        return dp[n];
+        
+        if(cache[n] != -1) return cache[n];
+        
+        int numbers = Integer.MAX_VALUE;
+        
+        
+        for(int i = 1; i*i<=n; i++) {
+            int square = i*i;
+            int diff = n - square;
+            int ways = 1 + numSquaresHelper(diff);
+            
+            numbers = Math.min(ways, numbers);
+        }
+        
+        return cache[n] = numbers;
     }
 }
